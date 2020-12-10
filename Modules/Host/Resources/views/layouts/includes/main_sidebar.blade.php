8e544f3628a5de52717c7744166694ac4fa4ffb7
duycuong96 <vuduycuong996@gmail.com>
2020-12-10 14:00:57 +0700

    commit

--- Modules/Host/Resources/views/layouts/includes/main_sidebar.blade.php
+++ Modules/Host/Resources/views/layouts/includes/main_sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="" class="brand-link">
-        <span class="brand-text font-weight-light">Thue phong tro</span>
+        <span class="brand-text font-weight-light">POLYHOME</span>
     </a>
 
     <!-- Sidebar -->
